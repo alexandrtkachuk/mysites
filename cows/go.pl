5b1507d5dca4f05361afa1e769f2b97f7a650d63
alexandr <assemblertai@gmail.com>
2016-02-08 19:05:40 +0200

    for sasha

--- cows/go.pl
+++ cows/go.pl
@@ -238,63 +238,90 @@ sub game
 	return 0;	
 }
 
-    
+
 sub perhaps
 {
-    my ($result, $values, @retArr) = @_;
+    my ($result, $values, @arr) = @_;
+	
+	my (@retArr);
 
-    my $count = @$values;
-    
-    my $amount = 0;
+    my $count = @arr; 
+	
+	if ($count)
+	{
+		(@retArr) = perhapsMin($result, $values, @arr);
+	}
+	else
+	{
+		(@retArr) = perhapsAll($result, $values, @arr);
+	}
 
-    for my $x (@DIDGITS)
-    {
-        my ($flag) = 0;
-
-        for my $i (0..$count - 1)
-        {
-            my $st = cow($x, $$values[$i]);
-
-            if(
-                !($st->{'cow'} eq $$result[$i]->{'cow'}) ||
-                !($st->{'bull'} eq $$result[$i]->{'bull'})
-            )
-            {
-                $flag = 1;
-                last;
-            }
-        }
-        
-        if (!$flag)
-        {     
-            push @retArr, $x;
-        }  
-    }
-    
     return (@retArr);
 }
+
 
 sub game2
 {
     my ($origin) = @_;
-    
-    my (@values) = ('1234', '4567', '3480', '6043');
-    
-    my (@result);
-    
-    for (@values)
-    {
-        my $st = cow($origin, $_);
 
-        push @result, $st;
-    }
+    #getFourValues();
     
-    perhaps(\@result, \@values);
+    my ($steps, @result, @resArr, @values) = (0);
+	
+	#'1234', '4567', '3480', '6043'
+
+    for ('1254', '4360', '5487', '6745')
+	{
+		$steps ++;
+
+		my $st = cow($origin, $_);
+
+		return ($_, $steps) if (4 == $st->{'bull'});	
+
+		push @result, $st;
+		push @values, $_;	
+		#(@resArr) = perhaps(\@result, \@values, @resArr);
+	}
     
+    (@resArr) = perhaps(\@result, \@values);
+
+	my $count =  @resArr;
+
+	if ( 1 == $count)
+	{
+		return (@resArr, $steps);
+	}
+	elsif(!$count)
+	{
+		return "error\n";
+	}
+	else
+	{
+		for(0..10)
+		{
+			$steps++;
+
+			my $r = int(rand($count));
+			
+			my $st = cow($origin, $resArr[$r]);
+			
+			push @result, $st;
+			push @values, $resArr[$r];
+
+			(@resArr) = perhaps(\@result, \@values, @resArr);
+
+			$count =  @resArr;	
+
+			return (@resArr, $steps) if (1 == $count);
+		}
+	}
+
+	return "none\n" ;
 }
 
 sub main
 {
+	
 	bustCustumCharactersRecursion([1,2,3,4,5,6,7,8,9,0], '', 4 );	
 
 	my $count = @DIDGITS;	
@@ -302,33 +329,94 @@ sub main
 	my (@statistic);
 	#statistics
     
-    #game2('8956');
-    
-    perhaps(
-        [ {'cow'=> 1, 'bull' => 0}, {'cow'=> 0, 'bull' => 0}, {'cow'=> 2, 'bull' => 1}, {'cow'=> 3, 'bull' => 1}]
-        , ['9612', '1025', '4786', '4398'] );
+    #game2('8956'); 
+	
+	my ($m) = 6000;
+	my ($s) = 1;
 
-    
-	for	(1..0)
+	my ($five, $min, $max) = (0,0,0);
+	my ($maximumSt, $nogood) = (0,0); # $maximumSt - подряд печальки
+
+	for	(0..100000)
     {
         my $r = int(rand($count));
+
+		#print "m:", $m, , " l:", $_ ," \n";
         #origin =6845 start=5241
         #game('6845');	
         #print $DIDGITS[$r], "\n";
 
-        my ($result, $step) = game($DIDGITS[$r]);
-
+		my ($result, $step) = game2($DIDGITS[$r]);
+		
+		$m = $m - $s;
+		
         if($result)
         {
-            #print $result ,' - ', $step ,"\n";
-
+			#print $result ,' - ', $step ,"\n";
+			$five++ if (5 == $step);
+			$min++ if (5 > $step);
+			$max++ if (5 < $step);
+			
             push @statistic, $step;
+
+			if (5 >= $step)
+			{
+				$m += $s * 1.5;	
+
+				$s = 1;	
+
+				$nogood = 0;
+			}
+			else
+			{
+				if($s == 1)
+				{
+					$s = 5;
+				}
+				elsif($s == 5)
+				{
+					$s = 50;
+				}
+				elsif($s == 50)
+				{
+					$s = 150;
+				}
+				elsif($s == 150)
+				{
+					$s = 250;
+				}
+				elsif($s == 250)
+				{
+					$s = 500;
+				}
+				elsif($s == 500)
+				{
+					$s = 1000; 
+				}
+				elsif($s == 1000)
+				{
+					$s == 2500;
+				}
+				elsif($ == 2500)
+				{
+					$s = 5000;
+				}
+
+
+				$nogood++;
+
+				$maximumSt =$nogood  if ($nogood > $maximumSt);
+			}
+
         }
         else
         {
             #print "none \n";
         }
     }	
+	
+	print "m:", $m, "\n";
+	print "max b:$maximumSt \n";
 
     my ($sum) = 0;
 
@@ -340,9 +428,15 @@ sub main
     $count = @statistic;
     if($count)
     {
-        print "statistic:" , $sum/$count, "\n"; 
+        print "statistic:" , $sum/$count, "\n";
+
+		print "5:$five \t min:$min \t max:$max \n";	
     }
 
 }
 
+my $time = time;
+
 main();
+
+print "time:", time - $time, "\n";
