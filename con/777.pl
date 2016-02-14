d6c240e475f577b46342e5c99d4bf9318caa5bbd
aitkachuk <alexandrtai@gmail.com>
2016-02-15 00:09:07 +0200

    dsa

---
+++ con/777.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE: 777.pl
+#
+#        USAGE: ./777.pl  
+#
+#  DESCRIPTION: 
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: YOUR NAME (), 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 09.02.2016 23:42:44
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use utf8;
+
+
+sub fun
+{
+    my ($didgit) = @_;
+
+    my @arr=split(//,$didgit);
+
+    if(777 == $didgit)
+    {
+        
+    }
+
+}
+
+
+for (1..100)
+{
+    my ($didgit) = int(rand(1000));
+
+    
+
+}
